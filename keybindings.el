7c4a4dbb38c836e7a5b5828a9d5b1c41da57a827
Matthew Pavlinsky <mpavlinsky@gmail.com>
2014-11-09 23:07:45 -0500

    Remove extra company mode keybindings.

--- keybindings.el
+++ keybindings.el
@@ -266,15 +266,12 @@
 (key-chord-define-global "l;" 'eval-expression)
 (key-chord-define-global "b;" 'mp-flycheck-dwim)
 
-
 ;; Error navigation / debugging
 (key-chord-define-global "kn" 'flycheck-next-error)
 (key-chord-define-global "ky" 'flycheck-previous-error)
 
 ;; Company
 (global-set-key (kbd "C-SPC") 'company-complete)
-(define-key company-mode-map (kbd "s-j") 'company-select-next)
-(define-key company-mode-map (kbd "s-k") 'company-select-previous)
 (define-key company-mode-map (kbd "C-s") 'company-filter-candidates)
 
 (provide 'keybindings)
